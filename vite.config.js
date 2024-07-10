4173c87da16e35980ab8947723dd00c39d6df223
Thekiso <diogoassuncao@msn.com>
2024-07-10 15:51:59 +0200

    Arreglar el build de la aplicación

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,15 @@
+import { resolve } from 'path'
 import { defineConfig } from "vite";
 
 export default defineConfig({
     plugins: [],
     base: "/Draco-Reign-Training-Time/",
+    build: {
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, 'index.html'),
+                timer: resolve(__dirname, 'src/pages/timer.html')
+            }
+        }
+    }
 });
